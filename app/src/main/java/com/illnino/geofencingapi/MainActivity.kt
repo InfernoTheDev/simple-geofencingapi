fd863bb01394241d06347f24fec29170524551a9
illnino <parinya.k09@gmail.com>
2018-05-06 15:23:29 +0700

    Display status at fragment

--- app/src/main/java/com/illnino/geofencingapi/MainActivity.kt
+++ app/src/main/java/com/illnino/geofencingapi/MainActivity.kt
@@ -2,22 +2,19 @@ package com.illnino.geofencingapi
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.app.IntentService
 import android.app.PendingIntent
-import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.location.Location
-import android.location.LocationManager
-import android.location.LocationProvider
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.widget.TextView
 import android.widget.Toast
 import com.google.android.gms.location.*
-import com.illnino.geofencingapi.R.id.toolbar
 import kotlinx.android.synthetic.main.activity_main.*
-import java.security.Provider
+import kotlinx.android.synthetic.main.fragment_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,10 +25,78 @@ class MainActivity : AppCompatActivity() {
     private lateinit var locationCallback: LocationCallback
     private lateinit var geofencingClient: GeofencingClient
     private lateinit var locationRequest: LocationRequest
-    var locationManager: LocationManager? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        geofencingClient = LocationServices.getGeofencingClient(this)
+        locationRequest = createLocationRequest()
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                locationResult ?: return
+                for (location in locationResult.locations){
+                    Log.d(TAG, "onLocationResult: ${location?.latitude}, ${location?.longitude}")
+                    updateDisplayView("onLocationResult: ${location?.latitude}, ${location?.longitude}\n")
+                }
+            }
+        }
+
+        val permission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+
+        if (permission == PackageManager.PERMISSION_GRANTED) {
+            //Toast.makeText(this, "Add Geofencing !!", Toast.LENGTH_LONG).show()
+            updateDisplayView("Add Geofencing !!")
+            Log.d(TAG, "Add Geofencing !!")
+
+            geofencingClient.addGeofences(getGeofencingRequest(), geofencePendingIntent).run {
+
+                addOnSuccessListener {
+                    //Toast.makeText(applicationContext, "geofencingClient add success", Toast.LENGTH_LONG).show()
+                    Log.d(TAG, "geofencingClient add success")
+                    updateDisplayView("geofencingClient add success")
+                }
+                addOnFailureListener {
+                    //Toast.makeText(applicationContext, "geofencingClient add fail ${it.message}", Toast.LENGTH_LONG).show()
+                    Log.d(TAG, "geofencingClient add fail ${it.message}")
+                    updateDisplayView("geofencingClient add fail ${it.message}")
+                }
+            }
+
+        } else {
+            Toast.makeText(this, "Permission Denied !!", Toast.LENGTH_LONG).show()
+            updateDisplayView("Permission Denied !!")
+        }
+
+    }
+
+    fun updateDisplayView(txt: String){
+        tv_display_status.append(txt + "\n")
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        startLocationUpdates()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        stopLocationUpdates()
+    }
+
+    private fun stopLocationUpdates() {
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
+
 
     private val geofencePendingIntent: PendingIntent by lazy {
         Toast.makeText(this, "Add Pending Intent", Toast.LENGTH_LONG).show()
+        updateDisplayView("\nAdd Pending Intent !!")
         //val intent = Intent("com.illnino.geofencingapi.ACTION_RECEIVE_GEOFENCE")
         val intent = Intent(this, GeofenceTransitionsIntentService::class.java)
         // We use FLAG_UPDATE_CURRENT so that we get the same pending intent back when calling
@@ -48,7 +113,7 @@ class MainActivity : AppCompatActivity() {
                 .setCircularRegion(
                         12.897571,
                         100.903045,
-                        10F
+                        20F
                 )
                 .setExpirationDuration(Geofence.NEVER_EXPIRE)
                 .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER or Geofence.GEOFENCE_TRANSITION_EXIT)
@@ -61,40 +126,6 @@ class MainActivity : AppCompatActivity() {
             setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER)
             addGeofences(geofenceList)
         }.build()
-    }
-
-    val locationListeners = arrayOf(
-            LTRLocationListener(LocationManager.GPS_PROVIDER),
-            LTRLocationListener(LocationManager.NETWORK_PROVIDER)
-    )
-
-    class LTRLocationListener(provider: String) : android.location.LocationListener {
-
-        val lastLocation = Location(provider)
-
-        override fun onLocationChanged(location: Location?) {
-            lastLocation.set(location)
-
-            Log.d(TAG, "onLocationChanged: ${location?.latitude}, ${location?.longitude}")
-            // TODO: Do something here
-        }
-
-        override fun onProviderDisabled(provider: String?) {
-        }
-
-        override fun onProviderEnabled(provider: String?) {
-        }
-
-        override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
-            Log.d(TAG, "onStatusChanged $provider, status: $status")
-
-            if (extras != null) {
-                for (i in extras.keySet()) {
-                    Log.d(TAG, "extras: ${extras[i]}")
-                }
-            }
-        }
-
     }
 
     fun createLocationRequest(): LocationRequest {
@@ -114,79 +145,63 @@ class MainActivity : AppCompatActivity() {
                         null)
     }
 
-    override fun onResume() {
-        super.onResume()
-        startLocationUpdates()
-    }
+    class GeofenceTransitionsIntentService : IntentService("GeofenceTransitionsIntentService") {
 
-    override fun onPause() {
-        super.onPause()
-        stopLocationUpdates()
-    }
+        override fun onHandleIntent(intent: Intent?) {
+        //override fun onReceive(context: Context?, intent: Intent?) {
+            Log.d(TAG, "GeofenceTransitionsIntentService Fire !!")
+            //updateDisplayView("GeofenceTransitionsIntentService Fire !!")
+            //display.append("GeofenceTransitionsIntentService Fire !!")
+            Toast.makeText(applicationContext, "GeofenceTransitionsIntentService Fire !!", Toast.LENGTH_LONG).show()
 
-    private fun stopLocationUpdates() {
-        fusedLocationClient.removeLocationUpdates(locationCallback)
-    }
+            val geofencingEvent = GeofencingEvent.fromIntent(intent)
+            if (geofencingEvent.hasError()) {
 
+                val errorMessage = GeofenceStatusCodes.getStatusCodeString(geofencingEvent.errorCode)
+                Toast.makeText(applicationContext, "GeofenceTransitionsIntentService errorMessage $errorMessage !!", Toast.LENGTH_LONG).show()
+                //display.append("GeofenceTransitionsIntentService errorMessage $errorMessage !!")
+                Log.e(TAG, errorMessage)
+                return
+            }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
+            // Get the transition type.
+            val geofenceTransition = geofencingEvent.geofenceTransition
 
-        // Create persistent LocationManager reference
-        /*if (locationManager == null)
-            locationManager = applicationContext.getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        try {
-            locationManager?.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0F, locationListeners[0])
-        } catch (e: SecurityException) {
-            Log.e(TAG, "Fail to request location update", e)
-        } catch (e: IllegalArgumentException) {
-            Log.e(TAG, "GPS provider does not exist", e)
-        }*/
+            // Test that the reported transition was of interest.
+            if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER ||
+                    geofenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT) {
 
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-        geofencingClient = LocationServices.getGeofencingClient(this)
-        locationRequest = createLocationRequest()
-
-        locationCallback = object : LocationCallback() {
-            override fun onLocationResult(locationResult: LocationResult?) {
-                locationResult ?: return
-                for (location in locationResult.locations){
-                    Log.d(TAG, "onLocationResult: ${location?.latitude}, ${location?.longitude}")
-                }
+                val status: String = handleEnterExit(geofenceTransition)
+                Log.d(TAG, status)
+                //display.append("Sucess: "+status)
+                Toast.makeText(applicationContext, "Sucess: "+status, Toast.LENGTH_LONG).show()
+            } else {
+                // Log the error.
+                //display.append("Error transition code: "+geofenceTransition)
+                Toast.makeText(applicationContext, "Error transition code: "+geofenceTransition, Toast.LENGTH_LONG).show()
+                Log.d(TAG, GeofenceStatusCodes.getStatusCodeString(geofenceTransition))
             }
         }
 
-        val permission = ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION)
+        private fun handleEnterExit(geofenceTransition: Int): String =
+                when(geofenceTransition){
 
-        if (permission == PackageManager.PERMISSION_GRANTED) {
-            Toast.makeText(this, "Add Geofencing !!", Toast.LENGTH_LONG).show()
-            Log.d(TAG, "Add Geofencing !!")
+                    Geofence.GEOFENCE_TRANSITION_ENTER -> {
+                        Log.d("geofence", "Entered")
+                        "Entered"
+                    }
 
-            geofencingClient.addGeofences(getGeofencingRequest(), geofencePendingIntent).run {
+                    Geofence.GEOFENCE_TRANSITION_EXIT ->{
+                        Log.d("geofence", "Exited")
+                        "Exited"
+                    }
 
-                addOnSuccessListener {
-                    //Toast.makeText(applicationContext, "geofencingClient add success", Toast.LENGTH_LONG).show()
-                    Log.d(TAG, "geofencingClient add success")
+                    else -> {
+                        Log.d("geofence", "Unknow")
+                        "Unknow"
+                    }
+
                 }
-                addOnFailureListener {
-                    //Toast.makeText(applicationContext, "geofencingClient add fail ${it.message}", Toast.LENGTH_LONG).show()
-                    Log.d(TAG, "geofencingClient add fail ${it.message}")
-                }
-            }
-
-        } else {
-            Toast.makeText(this, "Permission Denied !!", Toast.LENGTH_LONG).show()
-        }
-    }
-
-    //define the listener
-    private val locationListener: LocationListener = object : LocationListener {
-        override fun onLocationChanged(location: Location) {
-            //thetext.setText("" + location.longitude + ":" + location.latitude);
-        }
     }
 
 }
